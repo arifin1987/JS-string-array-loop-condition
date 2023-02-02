4d6786d76846046b26ce9d1f32884cddfde5cd11
Shohidul Arifin <shohidularifin@gmail.com>
2023-02-02 04:13:30 -0800

    finding max added

--- math.js
+++ math.js
@@ -1,8 +1,16 @@
 let num = 1.2345;
 let round = Math.round(num);
-console.log(round)
+// console.log(round)
 
 let num1 = -12;
 let abs = Math.abs(num1);
 
-console.log(abs);
+// console.log(abs);
+
+
+let karim =2;
+let rahim =5;
+let abir =45;
+
+let maxNum = Math.max(karim,rahim, abir);
+console.log(maxNum)
